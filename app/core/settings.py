473ab4e93e898d6b35ff65dd1d74714741213c8a
Adne Moretti <morettiadne1@gmail.com>
2025-07-02 13:34:44 -0300

    fix sonar issues

--- app/core/settings.py
+++ app/core/settings.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
 
     POSTGRES_SERVER: str = ""
     POSTGRES_PORT: int
-    POSTGRES_USER: str = "", 
+    POSTGRES_USER: str = ""
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
 
